75d7b3420460f75ba784f84e018b3496a3c36f50
augustrush <liupingwei30@gmail.com>
2015-07-11 16:38:00 +0800

    修改密码界面

---
+++ Lottery/Controllers/Modify Password/ModifyPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModifyPasswordViewController.h
+//  Lottery
+//
+//  Created by August on 15/7/11.
+//  Copyright (c) 2015年 August. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModifyPasswordViewController : UITableViewController
+
+@end
---
+++ Lottery/Controllers/Modify Password/ModifyPasswordViewController.m
@@ -0,0 +1,40 @@
+//
+//  ModifyPasswordViewController.m
+//  Lottery
+//
+//  Created by August on 15/7/11.
+//  Copyright (c) 2015年 August. All rights reserved.
+//
+
+#import "ModifyPasswordViewController.h"
+
+@interface ModifyPasswordViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *currentTextField;
+@property (weak, nonatomic) IBOutlet UITextField *theNewTextField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmTextField;
+
+@end
+
+@implementation ModifyPasswordViewController
+
+#pragma mark - lifeCycle methods
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)dealloc
+{
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - private methods
+
+#pragma mark -  event methdos
+
+
+@end
