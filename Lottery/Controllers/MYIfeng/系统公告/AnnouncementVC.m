c148943280d0760294b11e0d561c7f0710d36365
liujiyang <415302849@qq.com>
2015-07-26 22:11:52 +0800

    系统公告

--- Lottery/Controllers/MYIfeng/系统公告/AnnouncementVC.m
+++ Lottery/Controllers/MYIfeng/系统公告/AnnouncementVC.m
@@ -7,6 +7,8 @@
 //
 
 #import "AnnouncementVC.h"
+#import <MJRefresh.h>
+#import "HTTPClient+User.h"
 
 @interface AnnouncementVC ()<UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate>
 /**
@@ -27,24 +29,96 @@
  *  站内信
  */
 @property (weak, nonatomic) IBOutlet UITableView *insideLetterTable;
+/**
+ *  公告数据源
+ */
+@property (nonatomic, strong) NSMutableArray *announcementArray;
+/**
+ *  站内信数据源
+ */
+@property (nonatomic, strong) NSMutableArray *insidelLerrerArray;
 
 @end
 
 @implementation AnnouncementVC
 
+
+- (NSMutableArray *)announcementArray{
+    if (!_announcementArray) {
+        _announcementArray = [NSMutableArray arrayWithCapacity:10];
+    }
+    return _announcementArray;
+}
+
+- (NSMutableArray *)insidelLerrerArray{
+    if (!_insidelLerrerArray) {
+        _insidelLerrerArray = [NSMutableArray arrayWithCapacity:10];
+    }
+    return _insidelLerrerArray;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self setUpTableHeaderViewAndFooterView];
     
     [self.announcementTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
     [self.insideLetterTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"lcell"];
+    [self getDataFrom];
+}
+/**
+ *  添加下拉和上拉控件
+ */
+- (void)setUpTableHeaderViewAndFooterView{
     
+    __weak typeof(self) weakSelf = self;
+    // 设置公告的下拉刷新
+    MJRefreshNormalHeader *announheader = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [weakSelf.announcementArray removeAllObjects];
+//        [weakSelf getLotteryRecords];
+    }];
+    
+    // 设置自动切换透明度(在导航栏下面自动隐藏)
+    announheader.autoChangeAlpha = YES;
+    // 设置header
+    self.announcementTable.header = announheader;
+    //设置 站内信 下拉刷新
+    MJRefreshNormalHeader *insidHeader = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [weakSelf.insidelLerrerArray removeAllObjects];
+        //        [weakSelf getLotteryRecords];
+    }];
+    
+    // 设置自动切换透明度(在导航栏下面自动隐藏)
+    announheader.autoChangeAlpha = YES;
+    // 设置header
+    self.insideLetterTable.header = insidHeader;
+    
+    //添加上拉控件
     
 }
 
+
+- (void)getDataFrom{
+    [HTTPClient userHandleWithAction:24 paramaters:@{@"page": @"1",@"pageSize":@"20"} success:^(id task, id response) {
+        NSLog(@"%@",response);
+    } failed:^(id task, NSError *error) {
+        
+    }];
+}
+
+- (void)getAnnouncementData{
+    
+}
+
+- (void)getInsidelLerrerData{
+    
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 /**
